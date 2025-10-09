947bab732634879ef74e9d0f3d920b4af6b660c7
RosenKanev <26777365-RosenKanev@users.noreply.replit.com>
2025-10-09 10:30:29 +0000

    Restored to '42aa6424d5aba33bd17ba77acffed637cfef4b23'
    Replit-Restored-To: 42aa6424d5aba33bd17ba77acffed637cfef4b23

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -25,7 +25,7 @@ function Router() {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      <main className="flex-1 pb-20 md:pb-0">
+      <main className="flex-1 pb-16">
         <Switch>
           <Route path="/" component={Home} />
           <Route path="/login" component={Login} />
